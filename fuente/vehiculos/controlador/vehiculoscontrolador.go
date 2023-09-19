d2e766e4e2c5d57c19ea1fed4777e0c6315cf233
EliasMaDeRe <97937391+EliasMaDeRe@users.noreply.github.com>
2023-09-19 02:28:59 -0600

    Creando controlador y manejador de Vehiculos

---
+++ fuente/vehiculos/controlador/vehiculoscontrolador.go
@@ -0,0 +1,10 @@
+package controlador
+
+import "example/fleetwise/modelos/vehiculos"
+
+type Controlador struct {
+}
+
+func (c *Controlador) AgregarVehiculo(solicitud vehiculos.AgregarVehiculosSolicitud) {
+
+}
