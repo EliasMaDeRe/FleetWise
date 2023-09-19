b7118be07db2530ecfac8f85cd8a946ffa442b4e
CarlosJCD <carlos.calderon.d@icloud.com>
2023-09-19 13:04:44 -0600

    RF-001 Integracion con backend
    Integracion con backend para enviar los datos del formulario y esperar una respuesta para poder generar una alerta de exito o error

---
+++ js/capturaVehiculo.js
@@ -0,0 +1,75 @@
+(function () {
+  const botonEnviarFormularioCapturarVehiculo = document.querySelector(
+    ".captura-vehiculo__formulario-contenedor .formulario__submit"
+  );
+
+  if (botonEnviarFormularioCapturarVehiculo) {
+    botonEnviarFormularioCapturarVehiculo.addEventListener("click", (e) => {
+      e.preventDefault();
+      const inputsFormulario = obtenerInputsDelFormulario();
+      enviarFormulario(inputsFormulario);
+    });
+
+    function obtenerInputsDelFormulario() {
+      return document.querySelectorAll(".formulario__input");
+    }
+
+    async function enviarFormulario(inputsFormulario) {
+      const datosFormulario = construirPeticionFormulario(inputsFormulario);
+      const agregarVehículoUrl = "/AgregarVehiculo";
+      const peticion = await fetch(agregarVehículoUrl, {
+        method: "POST",
+        body: datosFormulario,
+      });
+      const respuesta = await peticion.json();
+      if (respuesta.Mensaje) {
+        desplegarAlerta("error", respuesta.Mensaje);
+      } else {
+        desplegarAlerta("exito", "Vehículo agregado exitosamente");
+      }
+    }
+
+    function construirPeticionFormulario(inputsFormulario) {
+      const datosFormulario = new FormData();
+      inputsFormulario.forEach((inputFormulario) => {
+        datosFormulario.append(
+          inputFormulario.name,
+          inputFormulario.textContent
+        );
+      });
+      return datosFormulario;
+    }
+
+    function desplegarAlerta(tipoDeAlerta, mensajeAlerta) {
+      const contenedorPrincipal = document.querySelector(
+        ".captura-vehiculo__contenedor"
+      );
+      const alertaExistente = contenedorPrincipal.querySelector(".alerta");
+      if (alertaExistente) {
+        contenedorPrincipal.removeChild(alertaExistente);
+      }
+      contenedorPrincipal.appendChild(
+        construirAlerta(tipoDeAlerta, mensajeAlerta)
+      );
+    }
+
+    function construirAlerta(tipoDeAlerta, mensajeDeAlerta) {
+      const contenedorAlerta = crearContenedorAlerta(tipoDeAlerta);
+      contenedorAlerta.append(crearTextoAlerta(mensajeDeAlerta));
+      return contenedorAlerta;
+    }
+
+    function crearContenedorAlerta(tipoDeAlerta) {
+      const contenedorAlerta = document.createElement("DIV");
+      contenedorAlerta.classList.add("alerta");
+      contenedorAlerta.classList.add(`alerta--${tipoDeAlerta}`);
+      return contenedorAlerta;
+    }
+
+    function crearTextoAlerta(mensajeDeAlerta) {
+      const textoAlerta = document.createElement("P");
+      textoAlerta.textContent = mensajeDeAlerta;
+      return textoAlerta;
+    }
+  }
+})();
