db641579ce5826fe72f7785a9e95f15fc3a164b8
Fernando <fernandvillajuana12@gmail.com>
2023-09-23 14:50:34 -0600

    agregé una constante

--- fuente/conectorbd/constantes/constantes.go
+++ fuente/conectorbd/constantes/constantes.go
@@ -5,4 +5,5 @@ const (
 	ERROR_CONECTAR_BD               = "Error: No se pudo conectar a la BD"
 	ERROR_PLACAS_INEXISTENTES_EN_BD = "Error: Placas no existen en la base de datos"
 	ERROR_SERIE_INEXISTENTES_EN_BD  = "Error: La serie no existe en la base de datos"
+	ERROR_GUARDAR_FILA_BD           = "Error: No se pudo guardar la fila en la BD"
 )
