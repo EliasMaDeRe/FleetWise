1cd15864b5b6b0fee5f7f80e2a416cc02da38ad5
EliasMaDeRe <97937391+EliasMaDeRe@users.noreply.github.com>
2023-10-24 06:18:46 -0600

    Pantalla front end para Tipo de servicio

---
+++ js/capturaServicioVehicular.js
@@ -0,0 +1,73 @@
+(function () {
+  const botonEnviarFormularioCapturarServicioVehicular = document.querySelector(
+    ".captura-servicio-vehicular__formulario-contenedor .formulario__submit"
+  );
+
+  if (botonEnviarFormularioCapturarServicioVehicular) {
+    botonEnviarFormularioCapturarServicioVehicular.addEventListener("click", (e) => {
+      e.preventDefault();
+      const inputsFormulario = obtenerInputsDelFormulario();
+      enviarFormulario(inputsFormulario);
+    });
+
+    function obtenerInputsDelFormulario() {
+      return document.querySelectorAll(".formulario__input");
+    }
+
+    async function enviarFormulario(inputsFormulario) {
+      const datosFormulario = construirPeticionFormulario(inputsFormulario);
+      const datosFormularioJSON = JSON.stringify(Object.fromEntries(datosFormulario));
+      const agregarServicioVehicularURL = "/AgregarServicioVehicular";
+      const peticion = await fetch(agregarServicioVehicularURL, {
+        method: "POST",
+        body: datosFormularioJSON,
+      });
+      const respuesta = await peticion.json();
+      if (!respuesta.OK) {
+        desplegarAlerta("error", respuesta.err);
+      } else {
+        desplegarAlerta("exito", "Servicio agregado exitosamente");
+      }
+    }
+
+    function construirPeticionFormulario(inputsFormulario) {
+      const datosFormulario = new FormData();
+      inputsFormulario.forEach((inputFormulario) => {
+        datosFormulario.append(inputFormulario.id, inputFormulario.value);
+      });
+      return datosFormulario;
+    }
+
+    function desplegarAlerta(tipoDeAlerta, mensajeAlerta) {
+      const contenedorPrincipal = document.querySelector(
+        ".captura-servicio-vehicular__contenedor"
+      );
+      const alertaExistente = contenedorPrincipal.querySelector(".alerta");
+      if (alertaExistente) {
+        contenedorPrincipal.removeChild(alertaExistente);
+      }
+      contenedorPrincipal.appendChild(
+        construirAlerta(tipoDeAlerta, mensajeAlerta)
+      );
+    }
+
+    function construirAlerta(tipoDeAlerta, mensajeDeAlerta) {
+      const contenedorAlerta = crearContenedorAlerta(tipoDeAlerta);
+      contenedorAlerta.append(crearTextoAlerta(mensajeDeAlerta));
+      return contenedorAlerta;
+    }
+
+    function crearContenedorAlerta(tipoDeAlerta) {
+      const contenedorAlerta = document.createElement("DIV");
+      contenedorAlerta.classList.add("alerta");
+      contenedorAlerta.classList.add(`alerta--${tipoDeAlerta}`);
+      return contenedorAlerta;
+    }
+
+    function crearTextoAlerta(mensajeDeAlerta) {
+      const textoAlerta = document.createElement("P");
+      textoAlerta.textContent = mensajeDeAlerta;
+      return textoAlerta;
+    }
+  }
+})();
