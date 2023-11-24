a22a684d3bcda0530c36109657f5600494d9cf6a
Fernando <fernandvillajuana123@gmail.com>
2023-11-24 01:23:52 -0600

    Base de Datos Registros
    Agregué la tabla de registros y también el html y js de registros

--- BD/gestor_de_flotillas.sql
+++ BD/gestor_de_flotillas.sql
@@ -56,13 +56,64 @@ INSERT INTO `servicios_vehiculares` (`nombre`) VALUES
 ('Llantas');
 
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gestor_de_flotillas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registros_mantenimiento_vehicular`
+--
+
+CREATE TABLE `registros_mantenimiento_vehicular` (
+  `numero_registro` int(11) NOT NULL,
+  `tipo_registro` varchar(255) NOT NULL,
+  `fecha` varchar(255) NOT NULL,
+  `litros_gasolina` float DEFAULT NULL,
+  `kilometraje` int(11) NOT NULL,
+  `importe` float NOT NULL,
+  `observaciones` varchar(255) DEFAULT NULL,
+  `concepto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `registros_mantenimiento_vehicular`
+--
+ALTER TABLE `registros_mantenimiento_vehicular`
+  ADD PRIMARY KEY (`numero_registro`);
+
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
+--
+-- AUTO_INCREMENT de la tabla `registros_mantenimiento_vehicular`
+--
+ALTER TABLE `registros_mantenimiento_vehicular`
+  MODIFY `numero_registro` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
 
 
 
