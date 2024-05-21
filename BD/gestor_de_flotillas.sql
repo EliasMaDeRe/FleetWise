543fdd3a46c7a9036ebf8ad706ebf609920d8f27
CarlosJCD <carlos.calderon.d@icloud.com>
2024-05-21 03:43:45 -0600

    Revert "Agrupamiento de Rutas Segun niveles de acceso"
    This reverts commit e11aa14a5cd10c887761dd03f34fbd8c22d60e66.

--- BD/gestor_de_flotillas.sql
+++ BD/gestor_de_flotillas.sql
@@ -192,10 +192,10 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Estructura Stand-in para la vista `v_registros_y_vehiculos_asociados`
+-- Estructura Stand-in para la vista `v_registros_y_vehiculos_aociados`
 -- (Véase abajo para la vista actual)
 --
-CREATE TABLE `v_registros_y_vehiculos_asociados` (
+CREATE TABLE `v_registros_y_vehiculos_aociados` (
 `fecha_lanzamiento` int(11)
 ,`marca` varchar(255)
 ,`modelo` varchar(255)
